910c6ade1996240a6903788b28aa1ec21bd0a6a5
Richard Amoah <amoahrichdad@gmail.com>
2025-09-14 17:52:55 +0000

    Basic Maths Operation

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+# Basic Maths Operations
+# Assign values to variables
+number1 = 10
+number2 = 5
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtration of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
